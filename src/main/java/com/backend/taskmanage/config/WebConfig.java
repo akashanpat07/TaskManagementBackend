91b576520af75b4e36a18ad864d98c46ac7b8a60
Akash Anpat <akashanpat1999@gmail.com>
2024-10-04 00:06:12 +0530

    first commit

---
+++ src/main/java/com/backend/taskmanage/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.backend.taskmanage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://taskmanagementapp-five.vercel.app") // Your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
